886bfeae00f41aab049313c1d57f013f49fad0c4
EerlijkeVerkiezingen <contact@eerlijkeverkiezingen.nl>
2015-03-23 15:26:33 +0000

    [2015-03-23] - auto update

--- update.sh
+++ update.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+git config --global user.name EerlijkeVerkiezingen
+git config --global user.mail contact@eerlijkeverkiezingen.nl
+
+git config --global -l
+
 git add .
 git commit -m "[`date +%F`] - auto update"
 git push
