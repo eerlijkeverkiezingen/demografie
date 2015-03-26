de2711cb7c6accc7bf6cee463e7a3fc412321d5e
EerlijkeVerkiezingen <contact@eerlijkeverkiezingen.nl>
2015-03-26 15:01:57 +0000

    [2015-03-26] mogelijk maken van manuele update en beargumentatie: note.txt

--- update.sh
+++ update.sh
@@ -6,8 +6,24 @@ git config --global -l
 
 git status 
 
+COMMIT=$1
+
+if [ -z "$COMMIT" ]; then
+	if [ -f note.txt ]; then
+	COMMIT="`cat note.txt`"
+	rm note.txt
+	fi
+fi
+
+if [ -z "$COMMIT" ]; then
+	COMMIT="auto update"
+fi
+
+# //echo $COMMIT
+# //exit
+
 if [ "`git status`" != "`cat ../unchanged-git-status`"  ]; then
 	git add .
-	git commit -m "[`date +%F`] auto update"
+	git commit -m "[`date +%F`] $COMMIT"
 	git push
 fi
